ef33d45ac63e5c3ee760d4bf58300fc8a17777c7
Dananprastya <88623767+Dananprastya@users.noreply.github.com>
2021-10-06 00:21:00 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RNqCUqqy7HMq6uR19FvJdr3PiB8H7fZ2sK.sikat1 -p x --cpu 4
